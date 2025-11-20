88e54fb74b5b87713d3649681b6ecf7d4a10a5cb
omarrabdoo00@gmail.com <omarrabdoo00@gmail.com>
2025-11-20 20:11:11 +0200

    Report Actions

--- __manifest__.py
+++ __manifest__.py
@@ -5,13 +5,18 @@
     'version': '17.0.0.1.0',
     'depends': ['base','sale','account','mail'],
     'data': [           # path within the application
+        # order is very important
+        #1- security
         'security/ir.model.access.csv',
+        #2- views
         'views/base_menu.xml',
         'views/property_view.xml',
         'views/owner_view.xml',
         'views/tag_view.xml',
         'views/sale_order_view.xml',
         'views/buidling_view.xml',
+        #3-Templates:
+        'reports/property_report.xml',
     ],
     'assets':{
         'web.assets_backend':['app_one/static/src/css/property.css']
